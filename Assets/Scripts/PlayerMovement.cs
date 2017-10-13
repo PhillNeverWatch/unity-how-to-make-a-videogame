0b54d77cae7f6e8f4d6896c33cf635f8b8dbdc65
Philipp Niebuhr <philipp_niebuhr@web.de>
2017-10-13 23:05:18 +0200

    added movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,16 +8,47 @@ public class PlayerMovement : MonoBehaviour {
     private float moveHorizontal;
     private float moveVertical;
     private Vector3 movement;
+    private float turningSpeed = 20f;
+    private Rigidbody playerRigidbody;
+
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+	{
+        // components from Player GameObject
+	    playerAnimator = GetComponent<Animator>();
+	    playerRigidbody = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+        // keyboard input
         moveHorizontal = Input.GetAxisRaw("Horizontal");
         moveVertical = Input.GetAxisRaw("Vertical");
-
         movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 	}
+
+    void FixedUpdate()
+    {
+        // If the player's movement vector does not equal zero...
+        if (movement != Vector3.zero)
+        {
+            // ...then create a target rotation based on the movement vector...
+            Quaternion targetRotation = Quaternion.LookRotation(movement, Vector3.up);
+
+            // ...and create another rotation that moves from the current rotation to the target rotation...
+            Quaternion newRotation = Quaternion.Lerp(playerRigidbody.rotation, targetRotation, turningSpeed * Time.deltaTime);
+
+            // ...and change the player's rotation to the incremental rotation...
+            playerRigidbody.MoveRotation(newRotation);
+
+            // ...then play the jump animation.
+            playerAnimator.SetFloat("Speed", 3f);
+        }
+        else
+        {
+            // Otherwise, don't play the jump animation
+            playerAnimator.SetFloat("Speed", 0f);
+        }
+    }
 }
