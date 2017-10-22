3ba7456397bcfad1fa15dbea5ef059fc7311b267
Philipp Niebuhr <philipp_niebuhr@web.de>
2017-10-22 20:14:55 +0200

    - follwing camera added - fly spawning and pickup animation added - fly respawn added

---
+++ Assets/Scripts/PickupParticlesDestriction.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupParticlesDestriction : MonoBehaviour {
+    
+	// Use this for initialization
+	void Start () {
+	    // Destroy the "pickup particles" after 5 seconds	
+	    Destroy(gameObject, 5f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
